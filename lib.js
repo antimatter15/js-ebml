c1d0b649891fc2684642591b10204452bd40e647
antimatter15 <antimatter15@gmail.com>
2010-10-02 19:20:03 +0000

    planned

--- lib.js
+++ lib.js
@@ -17,27 +17,6 @@ Schema Definition:
 Parsed JSON
 
 {
-  name: //name
-  id: //element id
-  mu: //multiple
-
-  data: {
-    //if type is master
-    Seek: [{ //use human readable names
-      //if type is multiple, use array
-    }]
-
-    EBMLVersion: 32
-  }
-
-  data: 'string' //if type string
-  data: 23423 //if type number
-  data: 'string' //type binary
-}
-
-
-
-{
   EBML: {
     EBMLVersion: 8,
     EBMLReadVersion: 8
