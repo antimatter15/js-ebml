47ad7e271efcc5d243bffc07fedc6470b20035d8
antimatter15 <antimatter15@gmail.com>
2010-10-03 21:25:31 +0000

    did stuff

---
+++ webm2.js
@@ -0,0 +1,266 @@
+[
+  {
+    "data": [
+      {
+        "data": "\u0001",
+        "name": "EBMLVersion",
+        "hex": "4286"
+      },
+      {
+        "data": "\u0001",
+        "name": "EBMLReadVersion",
+        "hex": "42f7"
+      },
+      {
+        "data": "\u0004",
+        "name": "EBMLMaxIDLength",
+        "hex": "42f2"
+      },
+      {
+        "data": "\u0008",
+        "name": "EBMLMaxSizeLength",
+        "hex": "42f3"
+      },
+      {
+        "data": "webm",
+        "name": "DocType",
+        "hex": "4282"
+      },
+      {
+        "data": "\u0002",
+        "name": "DocTypeVersion",
+        "hex": "4287"
+      },
+      {
+        "data": "\u0002",
+        "name": "DocTypeReadVersion",
+        "hex": "4285"
+      }
+    ],
+    "name": "EBML",
+    "hex": "1a45dfa3"
+  },
+  {
+    "data": [
+      {
+        "data": [
+          {
+            "data": [
+              {
+                "data": "\u0015I©f",
+                "name": "SeekID",
+                "hex": "53ab"
+              },
+              {
+                "data": "ß",
+                "name": "SeekPosition",
+                "hex": "53ac"
+              }
+            ],
+            "name": "Seek",
+            "hex": "4dbb"
+          },
+          {
+            "data": [
+              {
+                "data": "\u0016T®k",
+                "name": "SeekID",
+                "hex": "53ab"
+              },
+              {
+                "data": "\u0001,",
+                "name": "SeekPosition",
+                "hex": "53ac"
+              }
+            ],
+            "name": "Seek",
+            "hex": "4dbb"
+          },
+          {
+            "data": [
+              {
+                "data": "\u001cS»k",
+                "name": "SeekID",
+                "hex": "53ab"
+              },
+              {
+                "data": "\u0001ga",
+                "name": "SeekPosition",
+                "hex": "53ac"
+              }
+            ],
+            "name": "Seek",
+            "hex": "4dbb"
+          }
+        ],
+        "name": "SeekHead",
+        "hex": "114d9b74"
+      },
+      {
+        "data": "\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000",
+        "name": "Void",
+        "hex": "ec"
+      },
+      {
+        "data": [
+          {
+            "data": "\u000fB@",
+            "name": "TimecodeScale",
+            "hex": "2ad7b1"
+          },
+          {
+            "data": "Lavf52.79.0",
+            "name": "MuxingApp",
+            "hex": "4d80"
+          },
+          {
+            "data": "Lavf52.79.0",
+            "name": "WritingApp",
+            "hex": "5741"
+          },
+          {
+            "data": "T«h¿Y¬+ùRö\u000fö×C",
+            "name": "SegmentUID",
+            "hex": "73a4"
+          },
+          {
+            "data": "@D\u0000\u0000\u0000\u0000\u0000\u0000",
+            "name": "Duration",
+            "hex": "4489"
+          }
+        ],
+        "name": "Info",
+        "hex": "1549a966"
+      },
+      {
+        "data": [
+          {
+            "data": [
+              {
+                "data": "\u0001",
+                "name": "TrackNumber",
+                "hex": "d7"
+              },
+              {
+                "data": "\u0001",
+                "name": "TrackUID",
+                "hex": "73c5"
+              },
+              {
+                "data": "\u0000",
+                "name": "FlagLacing",
+                "hex": "9c"
+              },
+              {
+                "data": "und",
+                "name": "Language",
+                "hex": "22b59c"
+              },
+              {
+                "data": "V_VP8",
+                "name": "CodecID",
+                "hex": "86"
+              },
+              {
+                "data": "\u0001",
+                "name": "TrackType",
+                "hex": "83"
+              },
+              {
+                "data": "\u0002bZ\u0000",
+                "name": "DefaultDuration",
+                "hex": "23e383"
+              },
+              {
+                "data": [
+                  {
+                    "data": "\u000a\u0000",
+                    "name": "PixelWidth",
+                    "hex": "b0"
+                  },
+                  {
+                    "data": "\u0007",
+                    "name": "PixelHeight",
+                    "hex": "ba"
+                  },
+                  {
+                    "data": "\u000a\u0000",
+                    "name": "DisplayWidth",
+                    "hex": "54b0"
+                  },
+                  {
+                    "data": "\u0007",
+                    "name": "DisplayHeight",
+                    "hex": "54ba"
+                  },
+                  {
+                    "data": "\u0003",
+                    "name": "DisplayUnit",
+                    "hex": "54b2"
+                  }
+                ],
+                "name": "Video",
+                "hex": "e0"
+              }
+            ],
+            "name": "TrackEntry",
+            "hex": "ae"
+          }
+        ],
+        "name": "Tracks",
+        "hex": "1654ae6b"
+      },
+      {
+        "data": [
+          {
+            "data": "\u0000",
+            "name": "Timecode",
+            "hex": "e7"
+          },
+          {
+            "data": "SUPER MAGICAL",
+            "name": "SimpleBlock",
+            "hex": "a3"
+          }
+        ],
+        "name": "Cluster",
+        "hex": "1f43b675"
+      },
+      {
+        "data": [
+          {
+            "data": [
+              {
+                "data": "\u0000",
+                "name": "CueTime",
+                "hex": "b3"
+              },
+              {
+                "data": [
+                  {
+                    "data": "\u0001",
+                    "name": "CueTrack",
+                    "hex": "f7"
+                  },
+                  {
+                    "data": "\u0001",
+                    "name": "CueClusterPosition",
+                    "hex": "f1"
+                  }
+                ],
+                "name": "CueTrackPositions",
+                "hex": "b7"
+              }
+            ],
+            "name": "CuePoint",
+            "hex": "bb"
+          }
+        ],
+        "name": "Cues",
+        "hex": "1c53bb6b"
+      }
+    ],
+    "name": "Segment",
+    "hex": "18538067"
+  }
+]
