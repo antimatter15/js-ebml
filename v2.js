d5cc77344715589a86e4a289faf29d3b10c97c72
antimatter15 <antimatter15@gmail.com>
2010-10-03 21:24:25 +0000

    did stuff

--- v2.js
+++ v2.js
@@ -13,11 +13,24 @@
 */
 
 
-function toBinary(string){
+function toBinary(string, bytespace){
   return string.split('').map(function(i){
     var unpadded = i.charCodeAt(0).toString(2);
     return (new Array(8 - unpadded.length + 1)).join('0') + unpadded;
-  }).join('')
+  }).join(bytespace ? ' ' : '')
+}
+
+
+function HextoBinStr(bits){
+  var data = '';
+  //really, they're more of octets
+  if(bits.length % 2){
+    bits = '0' + bits;
+  }
+  for(var i = 0; i < bits.length; i+= 2){
+    data += String.fromCharCode(parseInt(bits.substr(i,2),16))
+  }
+  return data;
 }
 
 
@@ -37,6 +50,7 @@ function parseEBML(string){
   var offset = 0;
   var json = [];
   while(offset < string.length){
+    var startset = offset;
     var el_id = toBinary(string.substr(offset, 4));
     var segments = el_id.match(/^0*1/)[0].length;
     var id = el_id.substring(segments - 1, segments * 8);
@@ -46,6 +60,7 @@ function parseEBML(string){
     
     var el_size = toBinary(string.substr(offset, 8));
     var segments = el_size.match(/^0*1/)[0].length;
+    var sizestr = el_size.substring(0, segments * 8);
     var size = parseInt(el_size.substring(segments, segments * 8),2);
     
     offset += segments;
@@ -74,14 +89,18 @@ function generateEBML(json){
     
     var len = data.length;
     var zeroes = Math.ceil(Math.ceil(Math.log(len)/Math.log(2))/8);
-    //(zeroes + 1) * 8 - (zeroes + 1) = zeroes * 7 - 7 = needed size
     var size_str = len.toString(2);
     var padded = (new Array((zeroes * 7 + 7 + 1) - size_str.length)).join('0') + size_str;
-    var size = (new Array(zeroes + 1)).join('0') + '1' + padded;
+    var size = (new Array(zeroes)).join('0') + '1' + padded;
+    
 
-    ebml += toBinStr(parseInt(json[i].hex, 16).toString(2));
-    ebml += toBinStr(size);
-    ebml += data;
+    var element = '';
+    element += toBinStr(parseInt(json[i].hex, 16).toString(2));
+    element += toBinStr(size);
+    
+    element += data;
+    ebml += element;
+    
   }
   return ebml;
 }
