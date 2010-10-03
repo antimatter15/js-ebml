5eff7fb448d5ffb2fe237c2d1360e847a075fd6d
antimatter15 <antimatter15@gmail.com>
2010-10-03 14:21:40 +0000

    cool webp viewer

---
+++ webm.js
@@ -0,0 +1,85 @@
+{
+  "EBML": [
+    {
+      "EBMLVersion": 1,
+      "EBMLReadVersion": 1,
+      "EBMLMaxIDLength": 4,
+      "EBMLMaxSizeLength": 8,
+      "DocType": "webm",
+      "DocTypeVersion": 2,
+      "DocTypeReadVersion": 2
+    }
+  ],
+  "Segment": [
+    {
+      "SeekHead": [
+        {
+          "Seek": [
+            {
+              "SeekID": "\u0015I©f",
+              "SeekPosition": 223
+            },
+            {
+              "SeekID": "\u0016T®k",
+              "SeekPosition": 300
+            },
+            {
+              "SeekID": "\u001cS»k",
+              "SeekPosition": 42700
+            }
+          ]
+        }
+      ],
+      "Void": "\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000",
+      "Info": [
+        {
+          "TimecodeScale": 1000000,
+          "MuxingApp": "Lavf52.79.0",
+          "WritingApp": "Lavf52.79.0",
+          "SegmentUID": "Ü\u0001|ùV\u0012\u0015ª\u0010\u0004smã",
+          "Duration": "@D\u0000\u0000\u0000\u0000\u0000\u0000"
+        }
+      ],
+      "Tracks": [
+        {
+          "TrackEntry": [
+            {
+              "TrackNumber": 1,
+              "TrackUID": 1,
+              "FlagLacing": 0,
+              "Language": "und",
+              "CodecID": "V_VP8",
+              "TrackType": 1,
+              "DefaultDuration": 40000000,
+              "Video": {
+                "PixelWidth": 1231,
+                "PixelHeight": 847
+              }
+            }
+          ]
+        }
+      ],
+      "Cluster": [
+        {
+          "Timecode": 0,
+          "SimpleBlock": [
+            "MAGICKSTUFF"
+          ]
+        }
+      ],
+      "Cues": {
+        "CuePoint": [
+          {
+            "CueTime": 0,
+            "CueTrackPositions": [
+              {
+                "CueTrack": 1,
+                "CueClusterPosition": 373
+              }
+            ]
+          }
+        ]
+      }
+    }
+  ]
+}
